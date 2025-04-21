bb3af91857b5ecdf07126d2cb692ef3ed6c342ef
okstas86 <stanislavocunev1@gmail.com>
2025-04-21 16:36:50 +0300

    Fixed bugs

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,10 @@
-
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import FormDemo1 from '@/pages/FormDemo1.vue'
 import FormDemo2 from '@/pages/FormDemo2.vue'
 import FormDemo3 from '@/pages/FormDemo3.vue'
 import FormSuccess from '@/pages/FormSuccess.vue'
 
-const routes = [
+const routes: RouteRecordRaw[] = [
   { path: '/form1', component: FormDemo1 },
   { path: '/form2', component: FormDemo2 },
   { path: '/form3', component: FormDemo3 },
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'url'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/styles/variables.scss";`
-      }
+      '@': path.resolve(__dirname, 'src')
     }
   }
-  
 })
